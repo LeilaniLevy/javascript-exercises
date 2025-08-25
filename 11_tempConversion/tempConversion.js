5413bd317a904ae46d97595c37ba06e7ab4ffda9
Leilani Levy <224467588+LeilaniLevy@users.noreply.github.com>
2025-08-25 19:42:17 +0200

    Complete excercise

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let celsius = 0;
+  celsius = (fahrenheit - 32) / (9/5);
+  let celsiusRounded = Math.round(celsius * 10) / 10
+  return celsiusRounded;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let fahrenheit = 0;
+  fahrenheit = (celsius * (9/5)) + 32;
+  let fahrenheitRounded = Math.round(fahrenheit * 10) / 10
+  return fahrenheitRounded;
 };
 
 // Do not edit below this line
